e1cc71ce46e7423887e3ec5e450b2c84617992fe
Cesar Augusto Palacios Alarcon <ceapalaciosal@gmail.com>
2016-05-31 12:04:26 -0500

    Organización de Código

--- src/mainVP.py
+++ src/mainVP.py
@@ -18,98 +18,98 @@ from speciation import *
 from clear import *
 import os
 
-#folderout = os.path.join('..', 'data', 'out', '')
-#clear(folderout)
+folderout = os.path.join('..', 'data', 'out', '')
+clear(folderout)
 
-#archiveflows = os.path.join('..','data','flows','promFinal_1.csv'); #Archive traffic flow
+archiveflows = os.path.join('..','data','flows','promFinal_1.csv'); #Archive traffic flow
 
-#archiveprincipal = os.path.join('..','data','VP' , 'Principales','PRINCIPALES_1.xlsx');
-#archivesecondary = os.path.join('..','data','VP','Secundarias','SECUNDARIAS_1.xlsx');
-#archivetm = os.path.join('..','data','VP','TM','TM_1.xlsx')
+archiveprincipal = os.path.join('..','data','VP' , 'Principales','PRINCIPALES_1.xlsx');
+archivesecondary = os.path.join('..','data','VP','Secundarias','SECUNDARIAS_1.xlsx');
+archivetm = os.path.join('..','data','VP','TM','TM_1.xlsx')
 
-##flowsdays(archiveflows)
-#print 'comienza brinding hora para principales'
-#print '|==                                     |- 0%'
+#flowsdays(archiveflows)
+print 'comienza brinding hora para principales'
+print '|==                                     |- 0%'
 
-#folder = os.path.join('..','data','VP','Principales','')
-#binding (archiveflows, archiveprincipal, folder)
+folder = os.path.join('..','data','VP','Principales','')
+binding (archiveflows, archiveprincipal, folder)
 
-#print 'Finaliza proceso brinding hora principales' #End of main hour brinding process
-#print '|=========                              |- 25%'
+print 'Finaliza proceso brinding hora principales' #End of main hour brinding process
+print '|=========                              |- 25%'
 
-#print '------------------------------------------------------------------------'
-#print 'comienza brinding hora para secundarias' # Begining of secondary briniding process
-#print '|===================                    |- 50%'
-#bindingsecondary (archiveflows, archivesecondary)
-#print 'finaliza proceso brinding hora secundarias'
+print '------------------------------------------------------------------------'
+print 'comienza brinding hora para secundarias' # Begining of secondary briniding process
+print '|===================                    |- 50%'
+bindingsecondary (archiveflows, archivesecondary)
+print 'finaliza proceso brinding hora secundarias'
 
-#print '------------------------------------------------------------------------'
-#print 'comienza brinding hora para TM'
-#print '|===============================	       |- 75%'
-#folder = os.path.join('..','data','VP','TM','')
-#binding (archiveflows, archivetm, folder)
-#print 'finaliza proceso brinding hora TM'
-#print '|=======================================|- 100%'
+print '------------------------------------------------------------------------'
+print 'comienza brinding hora para TM'
+print '|===============================	       |- 75%'
+folder = os.path.join('..','data','VP','TM','')
+binding (archiveflows, archivetm, folder)
+print 'finaliza proceso brinding hora TM'
+print '|=======================================|- 100%'
 
-#archiveprincipal = os.path.join('..','data','VP','Principales','brinding.csv');
-#archivesecondary = os.path.join('..','data','VP','Secundarias','brinding.csv');
-#archivetm = os.path.join('..','data','VP','TM','brinding.csv');
+archiveprincipal = os.path.join('..','data','VP','Principales','brinding.csv');
+archivesecondary = os.path.join('..','data','VP','Secundarias','brinding.csv');
+archivetm = os.path.join('..','data','VP','TM','brinding.csv');
 
-##---------------------Vias Pavimentadas Principales-------------------------------------#
-#print 'Convirtiendo Excel a Matriz, para poder tratar los datos del Excel PRINCIPALES.xlsx'
-#matrizprincipal = convertCSVMatriz(archiveprincipal)
+#---------------------Vias Pavimentadas Principales-------------------------------------#
+print 'Convirtiendo Excel a Matriz, para poder tratar los datos del Excel PRINCIPALES.xlsx'
+matrizprincipal = convertCSVMatriz(archiveprincipal)
 
-#print 'Comienza Proceso para datos IDW Vias Principales Pavimentadas'
-#idw(matrizprincipal, archiveprincipal, 'Principal')
-
-
-##---No process in 2014-----#
-## print 'Comienza Proceso para datos CoKrigin Dia Habil Vias Principales Pavimentadas'
-## ckdh(matrizprincipal, archiveprincipal, 'Principal')
-
-## print 'Comienza Proceso para datos CoKrigin Dia No Habil Vias Principales Pavimentadas'
-## ckdnh(matrizprincipal, archiveprincipal, 'Principal')
-
-#print ''
-#print '----------------------------------------------------------------------------------'
-#print ''
-
-##---------------------Vias Pavimentadas TM-------------------------------------#
-#print 'Convirtiendo Excel a Matriz, para poder tratar los datos del Excel TM.xlsx'
-#matriztroncales = convertCSVMatriz(archivetm)
-
-#print 'Comienza Proceso para datos IDW Vias TM Pavimentadas'
-#idw(matriztroncales, archivetm,'TM')
+print 'Comienza Proceso para datos IDW Vias Principales Pavimentadas'
+idw(matrizprincipal, archiveprincipal, 'Principal')
 
 
-##------No process in 2014-----#
-## print 'Comienza Proceso para datos CoKrigin Dia Habil Vias TM Pavimentadas'
-## ckdh(matriztroncales, archivetm, 'TM')
+#---No process in 2014-----#
+# print 'Comienza Proceso para datos CoKrigin Dia Habil Vias Principales Pavimentadas'
+# ckdh(matrizprincipal, archiveprincipal, 'Principal')
 
-## print 'Comienza Proceso para datos CoKrigin Dia No Habil Vias TM Pavimentadas'
-## ckdnh(matriztroncales, archivetm, 'TM')
+# print 'Comienza Proceso para datos CoKrigin Dia No Habil Vias Principales Pavimentadas'
+# ckdnh(matrizprincipal, archiveprincipal, 'Principal')
 
-#print ''
-#print '----------------------------------------------------------------------------------'
-#print ''
+print ''
+print '----------------------------------------------------------------------------------'
+print ''
 
-##---------------------Vias Pavimentadas Secundarias-------------------------------------#
-#print 'Convirtiendo Excel a Matriz, para poder tratar los datos del Excel SECUNDARIAS.xlsx'
-#matrizsecondary = convertCSVMatriz(archivesecondary)
+#---------------------Vias Pavimentadas TM-------------------------------------#
+print 'Convirtiendo Excel a Matriz, para poder tratar los datos del Excel TM.xlsx'
+matriztroncales = convertCSVMatriz(archivetm)
 
-#print 'Comienza Proceso para datos IDW Vias Secundarias Pavimentadas'
-#secundaryIDW(matrizsecondary, archivesecondary, 'Secundary')
+print 'Comienza Proceso para datos IDW Vias TM Pavimentadas'
+idw(matriztroncales, archivetm,'TM')
 
-##-- No process in 2014---#
-## print 'Comienza Proceso para datos Homogeneos Vias Secundarias Pavimentadas'
-## secundaryHomogeneous(matrizsecondary, archivesecondary, 'Secundary')
 
-## print 'Comienza Proceso para datos Heterogeneos Vias Secundarias Pavimentadas'
-## secundaryHeterogeneous(matrizsecondary, archivesecondary, 'Secundary')
+#------No process in 2014-----#
+# print 'Comienza Proceso para datos CoKrigin Dia Habil Vias TM Pavimentadas'
+# ckdh(matriztroncales, archivetm, 'TM')
 
-#print '----------------------------------------------------------------------------------'
+# print 'Comienza Proceso para datos CoKrigin Dia No Habil Vias TM Pavimentadas'
+# ckdnh(matriztroncales, archivetm, 'TM')
 
-#emissiontotal()
+print ''
+print '----------------------------------------------------------------------------------'
+print ''
+
+#---------------------Vias Pavimentadas Secundarias-------------------------------------#
+print 'Convirtiendo Excel a Matriz, para poder tratar los datos del Excel SECUNDARIAS.xlsx'
+matrizsecondary = convertCSVMatriz(archivesecondary)
+
+print 'Comienza Proceso para datos IDW Vias Secundarias Pavimentadas'
+secundaryIDW(matrizsecondary, archivesecondary, 'Secundary')
+
+#-- No process in 2014---#
+# print 'Comienza Proceso para datos Homogeneos Vias Secundarias Pavimentadas'
+# secundaryHomogeneous(matrizsecondary, archivesecondary, 'Secundary')
+
+# print 'Comienza Proceso para datos Heterogeneos Vias Secundarias Pavimentadas'
+# secundaryHeterogeneous(matrizsecondary, archivesecondary, 'Secundary')
+
+print '----------------------------------------------------------------------------------'
+
+emissiontotal()
 
 print 'Start PMC'
 folderGrid = os.path.join('..','data','out', 'EmissionGrid', 'VP', '')
